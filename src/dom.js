439b17190c92735c2b3b8b437b65ddfd9c67ce94
Nasa28 <kaluchinasaagu@gmail.com>
2021-07-21 13:02:36 +0100

    add project folder button

---
+++ src/dom.js
@@ -0,0 +1,10 @@
+
+const dom = () => {
+const bookList = document.getElementById('book-list');
+const author = document.getElementById('author');
+const title = document.getElementById('title');
+const pages = document.getElementById('pages');
+const read = document.getElementById('status');
+const add = document.getElementById('btn-main');
+}
+export default dom
