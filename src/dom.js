95a2c3cbc0177f36a9555347ced685dfb404de3e
Nasa28 <kaluchinasaagu@gmail.com>
2021-07-25 13:34:01 +0100

    add local storage

--- src/dom.js
+++ src/dom.js
@@ -3,36 +3,35 @@ const title = document.getElementById('title');
 const description = document.getElementById('description');
 const dueDate = document.getElementById('dueDate');
 const name = document.getElementById('name');
-const todos = document.getElementById('proName');
 const priority = document.getElementById('priority')
  
 const h2 = document.createElement('h2');
 h2.textContent = "List of Pending todos"
-myLibrary =[]
+
 content.appendChild(h2)
 
 
 // Storage class 
 
 class Store {
-  static getTask(){
+  static getTodos(){
     let tasks;
-    if (localStorage.getItem('tasks') === null){
-      tasks = []
+    if(localStorage.getItem('tasks') === null){
+      tasks = [];
     }else {
       tasks = JSON.parse(localStorage.getItem('tasks'));
     }
     return tasks
   }
 
-  static addTasks(task){
-    const tasks = Store.getTask()
+  static addTodos(task){
+    const tasks = Store.getTodos()
     tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks))
   }
  
-  static deleteTask(title){
-    const tasks = Store.getTask()
+  static deleteTodos(title){
+    const tasks = Store.getTodos()
     tasks.forEach((task, index) =>{
       if (task.title === title){
         tasks.splice(index, 1)
@@ -62,40 +61,26 @@ class Task{
 //UI class goes in here
 
 class UI{
-  static displayTask(){
-    const storedTasks = [
-      {
-        title: "Get a coffee",
-        description: "Black coffee is the very best",
-        dueDate: "july 31 2021",
-        priority: "High"
-      },
-
-      {
-        title: "Get a coffee",
-        description: "Black coffee is the very best",
-        dueDate: "july 31 2021",
-        priority: "High"
-      }
-    ] 
-    const tasks = storedTasks;
-    tasks.forEach((task)=> UI.addTask(task))
+  static displayTask(){ 
+    const todos =Store.getTodos()
+    todos.forEach((task) => UI.addTask(task));
   }
+    
 
   static addTask(task){
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="card mb-5 border-3" style="width: 30rem;">
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">${task.title}</li>
-    <li class="list-group-item">${task.description}</li>
-    <li class="list-group-item">${task.dueDate}</li>
-    <li class="list-group-item">${task.priority}</li>
-  </ul>
-  <div class="card-footer">
-  <a href="#" class="delete text-danger text-decoration-none">Remove task</a>
-  </div>
-</div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${task.title}</li>
+        <li class="list-group-item">${task.description}</li>
+        <li class="list-group-item">${task.dueDate}</li>
+        <li class="list-group-item">${task.priority}</li>
+      </ul>
+      <div class="card-footer">
+        <a href="#" class="delete text-danger text-decoration-none">Remove task</a>
+      </div>
+    </div>
     `;
     content.appendChild(div)
   }
@@ -115,26 +100,18 @@ document.getElementById('main-form').addEventListener('submit', (event) => {
 
   // input Validation
 
-  if(
-    title.value ==='' ||
-    description.value === '' ||
-    dueDate.value === ''||
-    priority.value === ''
-  ){
-    alert('Please fill all fields')
-  }
-  else {
-    const task = new Task(
-      title.value,
-      description.value,
-      dueDate.value,
-      priority.value
-    );
-      UI.addTask(task)
-      Task.resetFields();
-  }
+  const task = new Task(
+    title.value,
+    description.value,
+    dueDate.value,
+    priority.value
+  );
+  UI.addTask(task) 
+  Store.addTodos(task) 
+  window.location.reload();
+  Task.resetFields();
+      
 
-  // window.location.reload();
 });
 
 // Add event listener for delete 
