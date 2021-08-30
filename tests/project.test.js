d3ac83cb50d299ea64c560173224640ac757b397
Nasa28 <kaluchinasaagu@gmail.com>
2021-08-30 23:54:38 +0100

    test presence of project class

--- tests/project.test.js
+++ tests/project.test.js
@@ -1,2 +1,9 @@
 import { expect, test } from '@jest/globals';
 import Project from '../src/project'
+
+describe('Project class', () =>{
+  test('Test if project class is defined', ()=>{
+    const project = new Project()
+    expect(project).toBeDefined();
+  })
+})
